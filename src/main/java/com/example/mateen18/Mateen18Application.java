2c9a54ed8f78d221d8c9fe86c862cff934f9f3cc
mateen <mateennandgadi@gmail.com>
2024-11-14 21:31:25 +0530

    okay

---
+++ src/main/java/com/example/mateen18/Mateen18Application.java
@@ -0,0 +1,13 @@
+package com.example.mateen18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mateen18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mateen18Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mateen18/Mateen18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mateen18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mateen18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
